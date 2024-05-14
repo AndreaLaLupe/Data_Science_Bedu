10041e2884abe31afe2bd2a2b6952ce683803838
AndreaLaLupe <aalvarez2014ig@gmail.com>
2024-05-14 21:22:32 +0000

    Reto #1

--- Sesion7.R
+++ Sesion7.R
@@ -3,3 +3,72 @@ library(gitcreds)
 
 gitcreds_set()
 gitcreds_get()
+
+install.packages("DBI")
+install.packages("RMySQL")
+
+library(DBI)
+library(RMySQL)
+
+MyDataBase <- dbConnect(
+  drv = RMySQL::MySQL(),
+  dbname = "shinydemo",
+  host = "shiny-demo.csa7qlmguqrf.us-east-1.rds.amazonaws.com",
+  username = "guest",
+  password = "guest")
+
+dbListTables(MyDataBase)
+
+dbListFields(MyDataBase, 'City')
+
+DataDB <- dbGetQuery(MyDataBase, "select * from City")
+
+class(DataDB)
+dim(DataDB)
+head(DataDB)
+
+pop.mean <- mean(DataDB$Population)  # Media a la variable de población
+pop.mean 
+
+pop.3 <- pop.mean *3   # Operaciones aritméticas
+pop.3
+
+library(dplyr)
+pop50.mex <-  DataDB %>% filter(CountryCode == "MEX" ,  Population > 50000)   # Ciudades del país de México con más de 50,000 habitantes
+
+head(pop50.mex)
+
+unique(DataDB$CountryCode)   # Países que contiene la BDD
+
+
+# Reto 1
+install.packages("DBI")
+install.packages("RMySQL")
+install.packages("dplyr")
+install.packages("ggplot2")
+library(dplyr)
+library(DBI)
+library(RMySQL)
+library(ggplot2)
+
+MyDataBase <- dbConnect(
+  drv = RMySQL::MySQL(),
+  dbname = "shinydemo",
+  host = "shiny-demo.csa7qlmguqrf.us-east-1.rds.amazonaws.com",
+  username = "guest",
+  password = "guest")
+
+dbListTables(MyDataBase)
+
+dbListFields(MyDataBase, 'CountryLanguage')
+
+DataDB <- dbGetQuery(MyDataBase, "select * from CountryLanguage")
+names(DataDB)
+
+SP <- DataDB %>% filter(Language == "Spanish")
+SP.df <- as.data.frame(SP) 
+
+
+SP.df %>% ggplot(aes( x = CountryCode, y=Percentage, fill = IsOfficial )) + 
+  geom_bin2d() +
+  coord_flip()
